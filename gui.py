5135fc6c2c5483c3b11eb8b13f7b18b6d55a7876
Ridvik Pal <pal.ridvik@gmail.com>
2023-06-11 20:02:50 -0400

    added row numbers and autofill to modify column

--- gui.py
+++ gui.py
@@ -13,13 +13,16 @@ def updateTableData() -> None:
         displayData.append(y)
 
 # update the table data and the actual gui
-def updateTableGUI() -> None:
+def updateTableGUI(window: pg.Window) -> None:
     updateTableData()
     window["-TABLE-"].update(values=displayData)
 
+# create the modify gui table
 def modifyGUI() -> None:
 
-    modifyHeader = [
+    # the header to
+    header = [
+        pg.Text("Row"),
         pg.Text("Account Type"),
         pg.Text("Account Number"),
         pg.Text("Transaction Date"),
@@ -27,15 +30,16 @@ def modifyGUI() -> None:
         pg.Text("Description")
     ]
 
-    modifyLayout = [modifyHeader]
+    modifyLayout = [header]
 
     for x in selectData:
         modifyLayout.append([
-            pg.Input(),
-            pg.Input(),
-            pg.Input(),
-            pg.Input(),
-            pg.Input(),
+            pg.Text(displayData.index(x)),
+            pg.Input(default_text=x[0]),
+            pg.Input(default_text=x[1]),
+            pg.Input(default_text=x[2]),
+            pg.Input(default_text=x[3]),
+            pg.Input(default_text=x[4])
         ])
 
     modifyWindow = pg.Window("Test", modifyLayout, resizable=True)
@@ -50,129 +54,135 @@ def modifyGUI() -> None:
 # this is the data that will be displayed on screen
 displayData = list()
 
-# update the table data for the first time upon program startup
-updateTableData()
-
 # this is the selected data row
 selectData = list()
 
-# headers for table
-header = [ "Account Type", "Account Number", "Transaction Date", "Amount", "Description" ]
+# define the main function
+def main() -> None:
+    # update the table data for the first time upon program startup
+    updateTableData()
 
-# create the table gui element
-mainTable = pg.Table(
-                values=displayData, headings=header,
-                auto_size_columns=True,
-                display_row_numbers=True,
-                justification='center',
-                select_mode='extended',
-                key='-TABLE-',
-                selected_row_colors='white on dark blue',
-                alternating_row_color="grey",
-                enable_events=True,
-                expand_x=True,
-                expand_y=True,
-                enable_click_events=True,
-                tooltip="Bank Transactions"
-            )
+    # headers for table
+    header = [ "Account Type", "Account Number", "Transaction Date", "Amount", "Description" ]
 
-# create manual entry gui element
-manualEntry = [
-    [pg.Text("Account Type: "), pg.Input(key='-ACC_TYPE-', justification='left')],
-    [pg.Text("Account Number: "), pg.Input(key='-ACC_NUM-', justification='left')],
-    [pg.Text("Transaction Date: "), pg.CalendarButton('Select', target='-TRANS_DATE-', format='%Y-%m-%d'), pg.Input(key='-TRANS_DATE-', justification='left')],
-    [pg.Text("Amount: "), pg.Input(key='-AMOUNT-', justification='left')],
-    [pg.Text("Description: "), pg.Input(key='-DESCRIPT-', justification='left')],
-    [pg.Button('Add Manual Entry', key='-MAN_ENTRY-')]
-]
+    # create the table gui element to show the SQL database
+    mainTable = pg.Table(
+                    values=displayData, headings=header,
+                    auto_size_columns=True,
+                    display_row_numbers=True,
+                    justification='center',
+                    select_mode='extended',
+                    key='-TABLE-',
+                    selected_row_colors='white on dark blue',
+                    alternating_row_color="grey",
+                    enable_events=True,
+                    expand_x=True,
+                    expand_y=True,
+                    enable_click_events=True,
+                    tooltip="Bank Transactions"
+                )
 
-# create delete entries button
-deleteEntries = [pg.Button("Delete Entries", key='-DELETE-')]
+    # create manual entry gui element
+    manualEntry = [
+        [pg.Text("Account Type: "), pg.Input(key='-ACC_TYPE-', justification='left')],
+        [pg.Text("Account Number: "), pg.Input(key='-ACC_NUM-', justification='left')],
+        [pg.Text("Transaction Date: "), pg.CalendarButton('Select', target='-TRANS_DATE-', format='%Y-%m-%d'), pg.Input(key='-TRANS_DATE-', justification='left')],
+        [pg.Text("Amount: "), pg.Input(key='-AMOUNT-', justification='left')],
+        [pg.Text("Description: "), pg.Input(key='-DESCRIPT-', justification='left')],
+        [pg.Button('Add Manual Entry', key='-MAN_ENTRY-')]
+    ]
 
-# create csv import button
-csvImport = [pg.Button('Import CSV File', key="-CSV-")]
+    # create delete entries button
+    deleteEntries = [pg.Button("Delete Entries", key='-DELETE-')]
 
-# create the modify button
-modifyEntriesButton = [pg.Button('Modify Entries', key='-MODIFY-')]
+    # create csv import button
+    csvImport = [pg.Button('Import CSV File', key="-CSV-")]
 
-# create the entire gui layout
-layout = [
-   [mainTable],
-   [manualEntry, csvImport],
-   [deleteEntries],
-   [modifyEntriesButton]
-]
+    # create the modify button
+    modifyEntriesButton = [pg.Button('Modify Entries', key='-MODIFY-')]
 
-# create the window
-window = pg.Window("SimplyTrack", layout, resizable=True)
+    # create the entire gui layout
+    layout = [
+        [mainTable],
+        [manualEntry, csvImport],
+        [deleteEntries],
+        [modifyEntriesButton]
+    ]
 
-# main event loop
-while True:
-    event, values = window.read() # read both events and values inputted into elements
-    # for debugging purposes, print the event and the values
-    # print("event:", event, "values:", values)
+    # create the window
+    window = pg.Window("SimplyTrack", layout, resizable=True)
 
-    # if the window is closed, break the main loop
-    if event == pg.WIN_CLOSED:
-        break
+    # main event loop
+    while True:
+        event, values = window.read() # read both events and values inputted into elements
+        # for debugging purposes, print the event and the values
+        # print("event:", event, "values:", values)
 
-    # if a table event has been clicked, store the clicked rows in selectData
-    if event == '-TABLE-':
-        selectData.clear()
-        for x in values['-TABLE-']:
-            selectData.append(displayData[x])
+        # if the window is closed, break the main loop
+        if event == pg.WIN_CLOSED:
+            break
 
-    # if csv import button has been clicked do something
-    if '-CSV-' in event:
-        # try to extract data from the csv file
-        try:
-            # get the raw csv file path
-            raw_path = pg.popup_get_file("Please choose a CSV File", title="Choose CSV File", file_types=(('CSV Files', '*.csv'),))
-            # check to make sure there is some raw path
-            if raw_path != None:
-                # check to make sure the path is not empty
-                if len(raw_path) > 0:
-                    # get a universal path, irrespective of os platform
-                    csv_file_path = Path(raw_path)
-                    # extract csv data into bank entry objects
-                    transactionsList = extract_csv.extractDataFromCSVFile(csv_file_path)
-                    # update those bank entry objects into sql database
-                    mysql_management.insertBulkDataIntoSQL(transactionsList)
-                    updateTableGUI()
-        # if there was some error, let the user know
-        except:
-            pg.popup_error("There was an error importing the csv file, please check the file path and/or csv file", title="An Error Occured")
+        # if a table event has been clicked, store the clicked rows in selectData
+        if event == '-TABLE-':
+            selectData.clear()
+            for x in values['-TABLE-']:
+                selectData.append(displayData[x])
 
-    # if a deletion was requested, delete the selected rows
-    if '-DELETE-' in event:
-        try:
-            for x in selectData:
-                accType, accNum, transDate, amount, description = x
-                mysql_management.deleteDataInSQL(accType, accNum, transDate, amount, description)
-            updateTableGUI()
-        except:
-            pg.popup_error("Please make sure you have selected an entry", title="An Error Occured")
+        # if csv import button has been clicked do something
+        if '-CSV-' in event:
+            # try to extract data from the csv file
+            try:
+                # get the raw csv file path
+                raw_path = pg.popup_get_file("Please choose a CSV File", title="Choose CSV File", file_types=(('CSV Files', '*.csv'),))
+                # check to make sure there is some raw path
+                if raw_path != None:
+                    # check to make sure the path is not empty
+                    if len(raw_path) > 0:
+                        # get a universal path, irrespective of os platform
+                        csv_file_path = Path(raw_path)
+                        # extract csv data into bank entry objects
+                        transactionsList = extract_csv.extractDataFromCSVFile(csv_file_path)
+                        # update those bank entry objects into sql database
+                        mysql_management.insertBulkDataIntoSQL(transactionsList)
+                        updateTableGUI(window)
+            # if there was some error, let the user know
+            except:
+                pg.popup_error("There was an error importing the csv file, please check the file path and/or csv file", title="An Error Occured")
 
-    # if a manual entry is entered into the system
-    if '-MAN_ENTRY-' in event:
-        try:
-            # first make sure all entries are filled
-            accType, accNum, transDate, amount, description = values['-ACC_TYPE-'], values['-ACC_NUM-'], values['-TRANS_DATE-'], values['-AMOUNT-'], values['-DESCRIPT-']
-            if values['-ACC_TYPE-'] and values['-ACC_NUM-'] and values['-TRANS_DATE-'] and values['-AMOUNT-'] and values['-DESCRIPT-']:
-                mysql_management.insertDataIntoSQL(accType,accNum, transDate, amount, description)
-                updateTableGUI()
-                # clear input fields
-                window['-ACC_TYPE-']('')
-                window['-ACC_NUM-']('')
-                window['-TRANS_DATE-']('')
-                window['-AMOUNT-']('')
-                window['-DESCRIPT-']('')
-            else:
-                pg.popup_error("Please make sure all fields are filled", title="An Error Occured")
-        except:
-            pg.popup_error("Please make sure all fields are filled with the correct data type", title="An Error Occured")
+        # if a deletion was requested, delete the selected rows
+        if '-DELETE-' in event:
+            try:
+                for x in selectData:
+                    accType, accNum, transDate, amount, description = x
+                    mysql_management.deleteDataInSQL(accType, accNum, transDate, amount, description)
+                updateTableGUI(window)
+            except:
+                pg.popup_error("Please make sure you have selected an entry", title="An Error Occured")
 
-    if '-MODIFY-' in event:
-        modifyGUI()
-# At the end of the program, close it
-window.close()
+        # if a manual entry is entered into the system
+        if '-MAN_ENTRY-' in event:
+            try:
+                # first make sure all entries are filled
+                accType, accNum, transDate, amount, description = values['-ACC_TYPE-'], values['-ACC_NUM-'], values['-TRANS_DATE-'], values['-AMOUNT-'], values['-DESCRIPT-']
+                if values['-ACC_TYPE-'] and values['-ACC_NUM-'] and values['-TRANS_DATE-'] and values['-AMOUNT-'] and values['-DESCRIPT-']:
+                    mysql_management.insertDataIntoSQL(accType,accNum, transDate, amount, description)
+                    updateTableGUI(window)
+                    # clear input fields
+                    window['-ACC_TYPE-']('')
+                    window['-ACC_NUM-']('')
+                    window['-TRANS_DATE-']('')
+                    window['-AMOUNT-']('')
+                    window['-DESCRIPT-']('')
+                else:
+                    pg.popup_error("Please make sure all fields are filled", title="An Error Occured")
+            except:
+                pg.popup_error("Please make sure all fields are filled with the correct data type", title="An Error Occured")
+
+        if '-MODIFY-' in event:
+            modifyGUI()
+    # At the end of the program, close it
+    window.close()
+
+# run the main function
+if __name__ == "__main__":
+    main()
